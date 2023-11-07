28cc408b88faedb868f99ec64145b887e778c7d7
Dzalex <milosdzaleta@live.com>
2023-11-07 18:33:30 +0100

    Implement power management for LTC

---
+++ sw/STM32CubeIDE_Project/Core/Inc/powerManagement.h
@@ -0,0 +1,24 @@
+/*
+ * powerManagement.h
+ *
+ *  Created on: Nov 7, 2023
+ *      Author: Dzale
+ */
+
+#ifndef INC_POWERMANAGEMENT_H_
+#define INC_POWERMANAGEMENT_H_
+#include "main.h"
+
+void LTC_Enable4VoltRail(void);
+void LTC_Disable4VoltRail(void);
+void LTC_SetBurstMode(void);
+void LTC_SetPulseSkipMode(void);
+void LTC_EnableHighPwrCharge(void);
+void LTC_DisableHighPwrCharge(void);
+void LTC_SuspendCharging(void);
+void LTC_ReactivateCharging(void);
+
+// TODO: Implement Charging status -->
+
+
+#endif /* INC_POWERMANAGEMENT_H_ */
---
+++ sw/STM32CubeIDE_Project/Core/Src/powerManagement.c
@@ -0,0 +1,47 @@
+/*
+ * powerManagement.c
+ *
+ *  Created on: Nov 7, 2023
+ *      Author: Dzale
+ */
+#include "powerManagement.h"
+
+void LTC_Enable4VoltRail(void)
+{
+	HAL_GPIO_WritePin(LTC_EN2_GPIO_Port, LTC_EN2_Pin, GPIO_PIN_SET);
+}
+
+void LTC_Disable4VoltRail(void)
+{
+	HAL_GPIO_WritePin(LTC_EN2_GPIO_Port, LTC_EN2_Pin, GPIO_PIN_RESET);
+}
+
+void LTC_SetBurstMode(void)
+{
+	HAL_GPIO_WritePin(LTC_MODE_GPIO_Port, LTC_MODE_Pin, GPIO_PIN_SET);
+}
+
+void LTC_SetPulseSkipMode(void)
+{
+	HAL_GPIO_WritePin(LTC_MODE_GPIO_Port, LTC_MODE_Pin, GPIO_PIN_RESET);
+}
+
+void LTC_EnableHighPwrCharge(void)
+{
+	HAL_GPIO_WritePin(LTC_HPWR_GPIO_Port, LTC_HPWR_Pin, GPIO_PIN_SET);
+}
+
+void LTC_DisableHighPwrCharge(void)
+{
+	HAL_GPIO_WritePin(LTC_HPWR_GPIO_Port, LTC_HPWR_Pin, GPIO_PIN_RESET);
+}
+
+void LTC_SuspendCharging(void)
+{
+	HAL_GPIO_WritePin(LTC_SUSP_GPIO_Port, LTC_SUSP_Pin, GPIO_PIN_SET);
+}
+
+void LTC_ReactivateCharging(void)
+{
+	HAL_GPIO_WritePin(LTC_SUSP_GPIO_Port, LTC_SUSP_Pin, GPIO_PIN_RESET);
+}
