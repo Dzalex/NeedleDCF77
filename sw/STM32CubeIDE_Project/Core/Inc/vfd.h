3fa72da46742c1e1ae01f5f5c61fd4c548f1b8fd
Dzalex <milosdzaleta@live.com>
2023-11-28 20:32:06 +0100

    Add Command 3 and Command 4 and signature of initialisation function

--- sw/STM32CubeIDE_Project/Core/Inc/vfd.h
+++ sw/STM32CubeIDE_Project/Core/Inc/vfd.h
@@ -10,17 +10,39 @@
 
 #include "main.h"
 
+/* General constants for PT6312 */
+const uint8_t PT6312_BYTES_PER_GRID = 2;
+const uint8_t VFD_GRIDS = 9;
+const uint8_t VFD_SEGMENTS = 8;
 
 /* COMMAND 1: DISPLAY MODE SETTING COMMANDS */
-const uint8_t DISPLAY_MODE_9DIG_13SEG = 0x05;
+const uint8_t VFD_DISPLAY_MODE_9DIG_13SEG = 0x05;
+
 
 /* COMMAND 2: DATA SETTING COMMANDS */
-const uint8_t DATA_SETTING_WRITE_TO_DISPLAY_MODE = 0x40;	/* Increment address after data has been written */
+const uint8_t VFD_DATA_SETTING_WRITE_TO_DISPLAY_MODE = 0x40;	/* Increment address after data has been written */
+
 /* Address increment mode settings (Display mode) - use as options | */
-const uint8_t INCREMENT_ADDRESS = 0x00;
-const uint8_t FIXED_ADDRESS = 0x04;
+const uint8_t VFD_INCREMENT_ADDRESS = 0x00;
+const uint8_t VFD_FIXED_ADDRESS = 0x04;
+
 /* Mode settings */
-const uint8_t TEST_MODE = 0x08;
+const uint8_t VFD_TEST_MODE = 0x08;
+
+
+/* COMMAND 3: ADDRESS SETTING COMMANDS 8 */
+const uint8_t VFD_ADDRESS_COMAND_BASE = 0xC0;	/* Address: 0x00 to 15
+
+
+/* COMMAND 4: DISPLAY CONTROL COMMANDS */
+const uint8_t VFD_BRIGHTNESS_BASE = 0x88;	/* Brightness 0 to 7 */
+const uint8_t VFD_OFF = 0x80;
+
+
+void VFD_PowerOnAndInitialize();
+
+
+
 
 
 #endif /* INC_VFD_H_ */
