8f4b1277c9196bba7a56d6f08ac60179dfd08927
Dzalex <milosdzaleta@live.com>
2023-12-03 21:30:53 +0100

    Implement VFD clear

--- sw/STM32CubeIDE_Project/Core/Src/vfd.c
+++ sw/STM32CubeIDE_Project/Core/Src/vfd.c
@@ -12,3 +12,15 @@ void VFD_Command(uint8_t command)
 	__HAL_SPI_DISABLE(&hspi1);
 }
 
+void VFD_Clear()
+{
+	VFD_Command(VFD_ADDRESS_COMAND_BASE | 0x00);
+
+	for(uint8_t i = 0; i < 0x16; i++)
+	{
+		HAL_SPI_Transmit(&hspi1, 0x00, 1, 100);
+	}
+
+	__HAL_SPI_DISABLE(&hspi1);
+}
+
