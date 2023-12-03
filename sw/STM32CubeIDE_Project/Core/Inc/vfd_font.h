3ee63c4c95f7f5a9aa714d930668451afd8189e8
Dzalex <milosdzaleta@live.com>
2023-12-03 21:07:27 +0100

    Add first font with numbers

---
+++ sw/STM32CubeIDE_Project/Core/Inc/vfd_font.h
@@ -0,0 +1,26 @@
+/*
+ * vfd_font.h
+ *
+ *  Created on: Nov 28, 2023
+ *      Author: Dzale
+ */
+
+#ifndef INC_VFD_FONT_H_
+#define INC_VFD_FONT_H_
+
+const uint8_t fontForVFD[] =
+{
+	0b11001111,		//0
+	0b00000110,		//1
+	0b00101101,		//2
+	0b00101111,		//3
+	0b01100110,		//4
+	0b01101011,		//5
+	0b11101011,		//6
+	0b00001110,		//7
+	0b11101111,		//8
+	0b01101111		//9
+};
+
+
+#endif /* INC_VFD_FONT_H_ */
