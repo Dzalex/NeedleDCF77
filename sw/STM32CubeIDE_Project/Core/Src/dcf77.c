5ebdbcf33c6c14c4dd78580941b6ca58b9b6423d
Dzalex <milosdzaleta@live.com>
2024-01-28 21:45:03 +0100

    Use union for DCF77 buffer

--- sw/STM32CubeIDE_Project/Core/Src/dcf77.c
+++ sw/STM32CubeIDE_Project/Core/Src/dcf77.c
@@ -19,23 +19,26 @@ const uint16_t ONE_PULS_DURATION_MIN = 140;
 const uint16_t MINUTE_MARK_PULS_DURATION_MAX = 2100;
 const uint16_t MINUTE_MARK_PULS_DURATION_MIN = 1600;
 
-struct DCF77Buffer_t {
-	uint64_t startOfMinute		:1;
-	uint64_t prefix     		:16;
-	uint64_t CEST       		:1;
-	uint64_t CET        		:1;
-	uint64_t leapSecond 		:1;
-	uint64_t startEncoding 		:1;
-	uint64_t Min        		:7;
-	uint64_t P1         		:1;
-	uint64_t Hour       		:6;
-	uint64_t P2         		:1;
-	uint64_t Day        		:6;
-	uint64_t Weekday    		:3;
-	uint64_t Month      		:5;
-	uint64_t Year       		:8;
-	uint64_t P3         		:1;
-    };
+typedef union {
+	struct {
+		 uint64_t startOfMinute		:1;
+		 uint64_t prefix     		:16;
+		 uint64_t CEST       		:1;
+		 uint64_t CET        		:1;
+		 uint64_t leapSecond 		:1;
+		 uint64_t startEncoding		:1;
+		 uint64_t Min        		:7;
+		 uint64_t P1         		:1;
+		 uint64_t Hour       		:6;
+		 uint64_t P2         		:1;
+		 uint64_t Day        		:6;
+		 uint64_t Weekday    		:3;
+		 uint64_t Month      		:5;
+		 uint64_t Year       		:8;
+		 uint64_t P3         		:1;
+    } DCF77Buffer_s;
+	uint64_t DCF77bits;
+} DCF77Buffer_t;
 
 enum PulseType {ZERO_PULSE = 0, ONE_PULSE = 1, MINUTE_PULSE, UNKNOWN_PULSE};
 
